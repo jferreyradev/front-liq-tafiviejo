6518df6218f94343615a1b1a616fe19346fc7b0f
Fernando Diaz Taballione <fdtaballione@gmail.com>
2026-01-14 09:57:36 -0300

    Fusion v02

--- src/router/index.js
+++ src/router/index.js
@@ -124,7 +124,17 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       // component: () => import('../views/ProcesosView.vue')
        component: () => import('../views/AcreditacionView.vue')
+    },
+        {
+      path: '/acredita',
+      name: 'acredita',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      // component: () => import('../views/ProcesosView.vue')
+       component: () => import('../views/AcreditacionesView.vue')
     }
+
 
   ]
 })
---
+++ src/views/AcreditacionView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import AcreditacionBase from '@/components/Acreditacion/AbreditacionBase.vue'
+
+</script>
+
+<template>
+  <v-container>
+    <v-row justify="space-around" class="bg-primary">
+      <h3>Acreditaciones</h3>
+    </v-row>
+    <v-row>
+      <AcreditacionBase></AcreditacionBase>
+    </v-row>
+
+  </v-container>
+</template>
